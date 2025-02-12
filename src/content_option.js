6787fd96bac21f4aa5ff8a33189e9099eec45b8b
Raj Patel <rajhpatel1174@gmail.com>
2025-02-12 00:01:25 -0800

    About Update

--- src/content_option.js
+++ src/content_option.js
@@ -33,7 +33,7 @@ const introdata = {
 const dataabout = {
     aboutme: "I am a problem-solver with a passion for technology, organization, and " +
              "efficiency. My skills include project management, technical problem-solving, " +
-             "communication, and customer service. I thrive in dynamic environments where I can" +
+             "communication, and customer service. I thrive in dynamic environments where I can " +
              "apply analytical thinking and adaptability to tackle challenges. With a strong " +
              "foundation in teamwork and leadership, I am always eager to learn, grow, and " +
              "contribute to meaningful projects.",
